01bc56611bfa0e48599713373840e0285130eadc
SergiiGrynevych <grinevich.sergey.official@gmail.com>
2020-09-05 11:13:59 +0300

    Добавлено еще одно возможное решение 3 задачи через reduce
    не подходит для решения задачи - но выводит ключ и значение лучшего работника

--- js/task_3.js
+++ js/task_3.js
@@ -6,24 +6,34 @@
 // Сотрудники и кол - во выполненных задач содержатся как свойства объекта в
 // формате "имя": "кол-во задач".
 
-const findBestEmployee = function (employees) {
-  "use strict";
+// ПЕРВЫЙ ВАРИАНТ РЕШЕНИЯ ///////////////////////////////////////////////////
+// const findBestEmployee = function (employees) {
+//   "use strict";
 
-  const entries = Object.entries(employees);
+//   const entries = Object.entries(employees);
 
-  if (entries.length !== 0) {
-    let bestEmployee = entries[0];
-    for (const employee of entries) {
-      if (employee[1] > bestEmployee[1]) {
-        bestEmployee = employee;
-      }
-    }
-    return bestEmployee[0];
-  }
-  return "";
-};
+//   if (entries.length !== 0) {
+//     let bestEmployee = entries[0];
+//     for (const employee of entries) {
+//       if (employee[1] > bestEmployee[1]) {
+//         bestEmployee = employee;
+//       }
+//     }
+//     return bestEmployee[0];
+//   }
+//   return "";
+// };
 
-// // Объекты и ожидаемый результат
+// // ВТОРОЙ ВАРИАНТ РЕШЕНИЯ ///////////////////////////////////////////////////
+// // c воспроизведением ключа и значения /////////////////////////////////////
+
+// const findBestEmployee = function (employees) {
+//   return Object.entries(employees).reduce((max, n) =>
+//     n[1] > max[1] ? n : max
+//   );
+// };
+
+// Объекты и ожидаемый результат
 const developers = {
   ann: 29,
   david: 35,
@@ -31,21 +41,24 @@ const developers = {
   lorence: 99,
 };
 console.log(findBestEmployee(developers));
-// // 'lorence'
+// 'lorence'
 
 const supports = {
   poly: 12,
   mango: 17,
   ajax: 4,
+  helen: 1,
+  helen: 1,
 };
 console.log(findBestEmployee(supports));
-// // 'mango'
+// 'mango'
 
 const sellers = {
-  lux: 147,
   david: 21,
   kiwi: 19,
+  lux: 147,
+  lux: 146,
   chelsy: 38,
 };
 console.log(findBestEmployee(sellers));
-// // 'lux'
+// 'lux'
